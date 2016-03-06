9c8650a8a5f85ad587f58b92d7eab9f13c15e67b
KvltKitty <jherzenach@mail.csuchico.edu>
2016-03-05 18:55:27 -0800

    Initial Push
    Added basic time control.  Start scene and press A to go forward in
    time, D to go back

---
+++ House/Assets/Scripts/Time/Timekeeper.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Timekeeper : MonoBehaviour {
+    private DateTime _time;
+	// Use this for initialization
+	void Start ()
+    {
+        _time = DateTime.Now;
+        Debug.Log(_time);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public DateTime getTime()
+    {
+        return _time;
+    }
+
+    public void setTime(bool clockWise)
+    {
+        if (clockWise)
+        { 
+            _time = _time.AddMinutes(1.0f);
+        }
+        else
+        {
+            _time = _time.AddMinutes(-1.0f);
+            //remove minutes
+        }
+    }
+}
