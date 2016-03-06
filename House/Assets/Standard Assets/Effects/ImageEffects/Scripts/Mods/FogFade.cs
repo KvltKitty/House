e28299b3eab6f16ae3d78b0cd76eb1813c1e4df3
KvltKitty <jherzenach@mail.csuchico.edu>
2016-03-06 03:43:08 -0800

    Made It Prettier!
    1) Added hand painting looking skybox
    2) Added neat fade out fog effect on play

---
+++ House/Assets/Standard Assets/Effects/ImageEffects/Scripts/Mods/FogFade.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FogFade : MonoBehaviour {
+    private UnityStandardAssets.ImageEffects.GlobalFog _globalFog;
+    private float height;
+	// Use this for initialization
+	void Awake () {
+        height = 20.0f;
+        _globalFog = gameObject.GetComponent<UnityStandardAssets.ImageEffects.GlobalFog>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(height >= 0.0f)
+        {
+            height -= (4.0f * Time.deltaTime);
+            _globalFog.height = height;
+        }
+        else
+        {
+            this.enabled = false;
+        }
+    }
+}
